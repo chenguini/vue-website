417afb43f5e719776abed66eeff03dd39954cd10
chenguini <chengn07@163.com>
2018-04-22 23:59:57 +0800

    添加编辑

---
+++ src/components/Loading.vue
@@ -0,0 +1,30 @@
+<template>
+	<div id="loading">
+		<img src="../../static/image/loading.gif">
+	</div>
+</template>
+
+<script>
+	export default {
+		name:'load',
+		data (){
+			return {
+
+			}
+		}
+	}
+</script>
+
+<style>
+	#loading {
+		text-align: center;
+		position: absolute;
+		top: 50%;
+		left: 50%;
+		transform: translate(-50%,-50%);
+	}
+	#loading img {
+		width: 40%;
+		height: 40%;
+	}
+</style>
---
+++ src/components/Pagination.vue
@@ -0,0 +1,79 @@
+<template>
+	<ul class="pagination" >
+		<li v-show="current != 1" @click="current-- && goto(current)" ><a href="#">上一页</a></li>
+		<li v-for="index in pages" @click="goto(index)" :class="{'active':current == index}" :key="index">
+			<a href="#" >{{index}}</a>
+		</li>
+		<li v-show="totalPage != current && totalPage != 0 " @click="current++ && goto(current++)"><a href="#" >下一页</a></li>
+	</ul>
+</template>
+
+<script>
+	export default {
+		name:'pagination',
+		data (){
+			return {
+				current:1,
+				showItem:5
+			}
+		},
+		props: ['page','totalPage'],
+		computed:{
+			pages:function(){
+				var pag = [];
+	            if( this.current < this.showItem ){ //如果当前的激活的项 小于要显示的条数
+	                   //总页数和要显示的条数那个大就显示多少条
+	                var i = Math.min(this.showItem,this.totalPage);
+	                while(i){
+	                   pag.unshift(i--);
+	                }
+	            }else{ //当前页数大于显示页数了
+	                var middle = this.current - Math.floor(this.showItem / 2 ),//从哪里开始
+	                i = this.showItem;
+	                if( middle >  (this.totalPage - this.showItem)  ){
+	                   	middle = (this.totalPage - this.showItem) + 1
+	                }
+	                while(i--){
+	                   pag.push( middle++ );
+	                }
+	            }
+	            return pag
+	        }
+        },
+     	methods:{
+        goto:function(index){
+          if(index == this.current) return;
+            this.current = index;
+            //这里可以发送ajax请求
+            this.$emit("listenToChildEvent",this.current);
+        }
+      }
+    }
+</script>
+
+<style scoped>
+	.pagination {
+		position: relative;
+		top: 50px;
+		text-align: center;
+	}
+	.pagination li {
+		display: inline-block;
+		margin:0 5px;
+	}
+	.pagination li a {
+		padding:.5rem 1rem;
+		display:inline-block;
+		border:1px solid #ddd;
+		background:#fff;
+
+		color:#0E90D2;
+	}
+	.pagination li a:hover {
+		background:#eee;
+	}
+	.pagination li.active a {
+		background:#0E90D2;
+		color:#fff;
+	}
+</style>
